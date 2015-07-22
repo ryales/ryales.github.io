052074689d062dc04cfaff594bbeb6639ec93c68
yacara15 <rcyales@gmail.com>
2015-07-23 00:13:35 +0100

    Migrate gh-pages from yacara15.github.io

---
+++ javascript/embarcs.js
@@ -0,0 +1,14 @@
+var embarcs = [
+{
+"type": "FeatureCollection",
+                                                                                
+"features": [
+{ "type": "Feature", "id": 0, "properties": { "mID": 1, "name": "Embarcadére de Maribarou", "altn1": null, "altn2": null, "map1": null, "text1": null, "map2": null }, "geometry": { "type": "Point", "coordinates": [ -71.760641604085691, 19.703572460790731 ] } }
+,
+{ "type": "Feature", "id": 1, "properties": { "mID": 2, "name": "Embarquadere de l'Abitation", "altn1": null, "altn2": null, "map1": 19, "text1": null, "map2": null }, "geometry": { "type": "Point", "coordinates": [ -73.686489769068245, 18.080513939037754 ] } }
+,
+{ "type": "Feature", "id": 2, "properties": { "mID": 3, "name": "Embarquadere d'Aquin", "altn1": null, "altn2": null, "map1": 67, "text1": null, "map2": 68 }, "geometry": { "type": "Point", "coordinates": [ -73.390940437361493, 18.279926165695368 ] } }
+
+]
+}
+]
---
+++ javascript/mapstyles.js
@@ -0,0 +1,54 @@
+//Map styles for Saint-Domingue.
+
+var geojsonMarkerOptions = {
+    radius: 5,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+var parishStyle = {
+    "color": "#677279",
+    "weight": 2,
+    "opacity": 1,
+    "fillOpacity": .4
+};
+
+var streetsStyle = {
+	"color": "#000000",
+    "weight": 1.5,
+    "opacity": 0.8,
+	"fillOpacity": .7
+};
+
+var propertyStyle = {
+    "fillColor": "#74CC74",
+	"color": "#677279",
+    "weight": 1,
+    "opacity": 0.8,
+	"fillOpacity": .7
+};
+
+var waterStyle = {
+    "color": "#0033CC",
+    "weight": 2,
+    "opacity": 1,
+	"fillOpacity": 1
+};	
+
+var bayStyle = {
+    "color": "#0033CC",
+    "weight": 0,
+    "opacity": 0,
+	"fillOpacity": .1
+};	
+
+var reefStyle = {
+    "color": "#5A5A5A",
+    "weight": 1,
+    "opacity": .3,
+	"dashArray": (10,4),
+	"fillOpacity":.1
+};
